5eafca6999aaa15dd72dcc5d1fe84176e3c4a0f0
rnlisboa <dev.renanlisboa@gmail.com>
2024-06-01 14:03:37 -0300

    style: remove argumento next de controller

--- backend/src/modules/user/controllers/CreateUserController.ts
+++ backend/src/modules/user/controllers/CreateUserController.ts
@@ -1,13 +1,12 @@
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 import CreateUserService from '../services/CreateUserService';
 
 class CreateUserController {
-    async handle(req: Request, res: Response, next: NextFunction) {
+    async handle(req: Request, res: Response) {
         const { ...user } = req.body;
         const createUserService = new CreateUserService();
         const userResponse = await createUserService.execute(user);
         return res.json(userResponse).status(401);
-
     }
 }
 
