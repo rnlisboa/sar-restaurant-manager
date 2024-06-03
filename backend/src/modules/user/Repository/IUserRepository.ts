7ff5136b7437f03efb4f8b45ed5b1509c97eca06
rnlisboa <dev.renanlisboa@gmail.com>
2024-06-03 13:10:14 -0300

    feat: implementa novos métodos

--- backend/src/modules/user/Repository/IUserRepository.ts
+++ backend/src/modules/user/Repository/IUserRepository.ts
@@ -1,6 +1,9 @@
 import { User } from "@prisma/client";
 
-export interface IUserRepository{
-    createUser: (user: User, passwordHash: string)=>Promise<User>;
-    findFirst: (email: string)=>Promise<User | null>;
+export interface IUserRepository {
+    getUserById: (id: string) => Promise<User | null>;
+    deleteUser: (id: string) => Promise<void>;
+    createUser: (user: User, passwordHash: string) => Promise<User>;
+    findFirst: (email: string) => Promise<User | null>;
+    updateUser: (id: string, user: Partial<User>) => Promise<User | null>;
 }
