8d613cf852be58ff2e583a84946244d63535a51d
rnlisboa <dev.renanlisboa@gmail.com>
2024-06-03 13:10:25 -0300

    feat: cria novas rotas de usaurio

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -2,10 +2,16 @@ import { Router } from "express";
 import uploadConfig from "../config/multer"
 import CreateUserController from "../modules/user/Controllers/CreateUserController";
 import multer from "multer";
+import UserDetailController from "../modules/user/Controllers/UserDetailController";
+import UpdateUserController from "../modules/user/Controllers/UpdateUserController";
+import DeleteUserController from "../modules/user/Controllers/DeleteUserController";
 
 const usersRouter = Router();
 const upload = multer(uploadConfig.upload('./tmp'))
-// Cadastra usuário
+// CRUD USUARIO
 usersRouter.post('/', upload.single('file'),new CreateUserController().handle);
+usersRouter.get('/:id', new UserDetailController().handle);
+usersRouter.put('/:id', upload.single("file"), new UpdateUserController().handle);
+usersRouter.delete('/:id', new DeleteUserController().handle);
 
 export default usersRouter;
